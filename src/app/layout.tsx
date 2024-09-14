cb655a877267c74305ccf3fde8b59cda671cb330
Nahida Zaman Bina <nahidazmn@gmail.com>
2024-09-14 11:04:51 +0600

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import StyledContext from "@context/StyledContext";
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bonik - The Best React eCommerce Template",
+  title: "Tizaraa - The Best React eCommerce Template",
   description:
     "Bonik is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store",
   authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
