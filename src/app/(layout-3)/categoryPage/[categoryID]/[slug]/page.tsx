98283ba58bd495bea773c06e4a166a1fa200d0dc
Nahida Zaman Bina <nahidazmn@gmail.com>
2024-09-19 13:36:54 +0600

    category page details

--- src/app/(layout-3)/categoryPage/[categoryID]/[slug]/page.tsx
+++ src/app/(layout-3)/categoryPage/[categoryID]/[slug]/page.tsx
@@ -1,20 +1,52 @@
+// import Box from "@component/Box";
+// import CategoryResult from "./CategoryResult";
+// import layout3 from "@component/layout/layout-3";
+
+// export default function ProductSearchResult() {
+//   return (
+//     <Box pt="20px">
+//       <CategoryResult sortOptions={sortOptions} />
+//     </Box>
+//   );
+// }
+
+// const sortOptions = [
+//   { label: "Relevance", value: "Relevance" },
+//   { label: "Date", value: "Date" },
+//   { label: "Price Low to High", value: "Price Low to High" },
+//   { label: "Price High to Low", value: "Price High to Low" }
+// ];
+
+
+
 import Box from "@component/Box";
 import CategoryResult from "./CategoryResult";
 import layout3 from "@component/layout/layout-3";
 
-export default function ProductSearchResult() {
+
+
+
+export default function ProductSearchResult({ params })
+{
+
   return (
-    <Box pt="20px">
-      <CategoryResult sortOptions={sortOptions} />
-    </Box>
+    <div>
+      {/* <h1>Category Page {params.id}</h1> */}
+
+      <CategoryResult categoryid={params.id}/>
+    </div>
   );
+  
 }
 
-const sortOptions = [
-  { label: "Relevance", value: "Relevance" },
-  { label: "Date", value: "Date" },
-  { label: "Price Low to High", value: "Price Low to High" },
-  { label: "Price High to Low", value: "Price High to Low" }
-];
+// const sortOptions = [
+//   { label: "Relevance", value: "Relevance" },
+//   { label: "Date", value: "Date" },
+//   { label: "Price Low to High", value: "Price Low to High" },
+//   { label: "Price High to Low", value: "Price High to Low" }
+// ];
 
 
+
+
+ 
