69b315fd8dfe27baf0523ca88da8ab988c00b423
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-10-17 16:13:38 +0600

    New Arrival product set

--- src/page-sections/market-1/NewArrivalsProducts/NewArrivalsProduct.tsx
+++ src/page-sections/market-1/NewArrivalsProducts/NewArrivalsProduct.tsx
@@ -14,8 +14,6 @@ import CategorySectionCreator from "@component/CategorySectionCreator";
 import { currency } from "@utils/utils";
 import useWindowSize from "@hook/useWindowSize";
 
-
-
 import ApiBaseUrl from "../../../api/ApiBaseUrl";
 import Rating from "@component/rating";
 import { Chip } from "@component/Chip";
@@ -53,30 +51,37 @@ export default function NewArrivalsProduct() {
   }, []);
 
   return (
-    <CategorySectionCreator title="New Arrivals" seeMoreLink="#">
+    <CategorySectionCreator title="New Arrivals" seeMoreLink={`newarrivals/NewArrivals`}>
       <Box my="-0.25rem">
-        <Carousel totalSlides={bigDiscountList.length} visibleSlides={visibleSlides}>
+        <Carousel
+          totalSlides={bigDiscountList.length}
+          visibleSlides={visibleSlides}
+        >
           {bigDiscountList.map((item) => (
             <Box py="0.25rem" key={item.product_slug}>
-              <Card p="1rem" borderRadius={8} style={{ height: '300px' }}>
-                 {/* Discount Badge */}
-              {!!item.discount_price && item.discount_price < item.seeling_price && (
-  <Chip
-    top="1rem"
-    left="1.2rem"
-    p="0.25rem 0.5rem"
-    fontSize="12px"
-    fontWeight="600"
-    bg="primary.main"
-    position="absolute"
-    color="primary.text"
-    zIndex={1}
-   
-    
-  >
-    {Math.floor(((item.seeling_price - item.discount_price) / item.seeling_price) * 100)}% off
-  </Chip>
-)}
+              <Card p="1rem" borderRadius={8} style={{ height: "300px" }}>
+                {/* Discount Badge */}
+                {!!item.discount_price &&
+                  item.discount_price < item.seeling_price && (
+                    <Chip
+                      top="1rem"
+                      left="1.2rem"
+                      p="0.25rem 0.5rem"
+                      fontSize="12px"
+                      fontWeight="600"
+                      bg="primary.main"
+                      position="absolute"
+                      color="primary.text"
+                      zIndex={1}
+                    >
+                      {Math.floor(
+                        ((item.seeling_price - item.discount_price) /
+                          item.seeling_price) *
+                          100
+                      )}
+                      % off
+                    </Chip>
+                  )}
                 <Link href={`/product/${item.product_slug}`}>
                   {/* <HoverBox
                     borderRadius={8}
@@ -93,56 +98,65 @@ export default function NewArrivalsProduct() {
                     />
                   </HoverBox> */}
                   <Box position="relative">
-              <img 
-                src={item.product_thumbnail} 
-                alt={item.product_name} 
-                style={{ width: '100%', borderRadius: '8px', objectFit: 'cover' }} 
-              />
-
-             
-
-            </Box>
+                    <img
+                      src={item.product_thumbnail}
+                      alt={item.product_name}
+                      style={{
+                        width: "100%",
+                        borderRadius: "8px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </Box>
 
                   <H4
                     fontWeight="600"
                     fontSize="18px"
                     mb="0.25rem"
                     style={{
-                      whiteSpace: 'nowrap',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
                     }}
                   >
                     {item.product_name}
                   </H4>
 
                   {item.rating > 0 && (
-  <Rating value={item.rating} outof={5} color="warn" readOnly />
-)}
+                    <Rating
+                      value={item.rating}
+                      outof={5}
+                      color="warn"
+                      readOnly
+                    />
+                  )}
 
+                  {item.discount_price == null && (
+                    <FlexBox>
+                      <H4 fontWeight="600" fontSize="14px" color="primary.main">
+                        {currency(item.seeling_price)}
+                      </H4>
+                    </FlexBox>
+                  )}
 
-{item.discount_price == null && (
-  <FlexBox>
-    <H4 fontWeight="600" fontSize="14px" color="primary.main">
-       {currency(item.seeling_price)}
-    </H4>
-  </FlexBox>
-)}
-
-{item.discount_price != null && (
-  <FlexBox flexDirection="column">
-    <H4 fontWeight="600" fontSize="14px" color="text.muted">
-     BDT <del>{(item.seeling_price)}</del>
-    </H4>
-    <Box marginTop="4px"> {/* Adjust margin as needed */}
-      <H4 fontWeight="600" fontSize="14px" color="primary.main">
-        {currency(item.discount_price)}
-      </H4>
-    </Box>
-  </FlexBox>
-)}
-
-
+                  {item.discount_price != null && (
+                    <FlexBox flexDirection="column">
+                      <H4 fontWeight="600" fontSize="14px" color="text.muted">
+                        BDT <del>{item.seeling_price}</del>
+                      </H4>
+                      <Box marginTop="4px">
+                        {" "}
+                        {/* Adjust margin as needed */}
+                        <H4
+                          fontWeight="600"
+                          fontSize="14px"
+                          color="primary.main"
+                        >
+                          {currency(item.discount_price)}
+                        </H4>
+                      </Box>
+                    </FlexBox>
+                  )}
                 </Link>
               </Card>
             </Box>
