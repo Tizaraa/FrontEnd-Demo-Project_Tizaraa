fd3b691ec63ea6f5fab039b18d0fe188af8ed956
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-10-30 15:43:42 +0600

    logo change tizaraa

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -29,7 +29,7 @@ export default function Topbar() {
       <Container display="flex" justifyContent="space-between" alignItems="center" height="100%">
         <FlexBox className="topbar-left">
           <div className="logo">
-            <img src="/assets/images/tizaraa_logo.webp" alt="logo" />
+            <img src="https://frontend.tizaraa.com/public/frontend/assets/img/site-logo/tizaraa-logo.png" alt="logo" />
           </div>
 
           <FlexBox alignItems="center">
