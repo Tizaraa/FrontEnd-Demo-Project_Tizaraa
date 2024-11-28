eb854c66cc6e7f8409757a4c2fe383a212e49a1d
Nahida Zaman Bina <nahidazmn@gmail.com>
2024-11-28 19:48:11 +0600

    logo responsive

--- src/components/header/CommonHeader.tsx
+++ src/components/header/CommonHeader.tsx
@@ -9,29 +9,13 @@ export default function CommonHeader() {
   return (
     <Link href="/">
       <div
-        style={{
-          position: "absolute",
-          top: "10px",
-          left: "10px",
-        }}
+        className="header-container"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         {/* Tooltip */}
         {isHovered && (
-          <div
-            style={{
-              position: "absolute",
-              top: "50px",
-              left: "30px",
-              backgroundColor: "black",
-              color: "white",
-              padding: "5px 10px",
-              borderRadius: "5px",
-              fontSize: "12px",
-              whiteSpace: "nowrap",
-            }}
-          >
+          <div className="tooltip">
             Home
           </div>
         )}
@@ -40,9 +24,53 @@ export default function CommonHeader() {
         <img
           src="/assets/images/tizaraa_logo.webp"
           alt="logo"
-          style={{ height: "50px", objectFit: "contain" }}
+          className="logo"
         />
       </div>
+
+      <style jsx>{`
+        .header-container {
+          position: absolute;
+          top: 10px;
+          left: 10px;
+        }
+
+        .tooltip {
+          position: absolute;
+          top: 50px;
+          left: 30px;
+          background-color: black;
+          color: white;
+          padding: 5px 10px;
+          border-radius: 5px;
+          font-size: 12px;
+          white-space: nowrap;
+        }
+
+        .logo {
+          height: 50px;
+          object-fit: contain;
+        }
+
+        @media (max-width: 768px) {
+          .header-container {
+            position: static;
+            display: flex;
+            justify-content: center;
+            margin: 10px 0;
+          }
+
+          .tooltip {
+            top: 60px;
+            left: 50%;
+            transform: translateX(-50%);
+          }
+
+          .logo {
+            margin: 0 auto;
+          }
+        }
+      `}</style>
     </Link>
   );
 }
