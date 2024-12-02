aff947750311d3abde14f9cf5b1806a44958738d
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-12-02 15:51:51 +0600

    footer icon design solve

--- src/components/footer/footer-1/Footer1.tsx
+++ src/components/footer/footer-1/Footer1.tsx
@@ -1,154 +1,396 @@
-import Link from "next/link";
+// import Link from "next/link";
 
+// import Box from "@component/Box";
+// import Image from "@component/Image";
+// import Grid from "@component/grid/Grid";
+// import Icon from "@component/icon/Icon";
+// import FlexBox from "@component/FlexBox";
+// import AppStore from "@component/AppStore";
+// import Container from "@component/Container";
+// import Typography, { Paragraph } from "@component/Typography";
+
+// // STYLED COMPONENTS
+// import { StyledLink } from "./styles";
+// // CUSTOM DATA
+// import { aboutLinks, customerCareLinks, customerCareLink, iconList } from "./data";
+
+// export default function Footer1() {
+//   return (
+//     <footer>
+//       {/* #0F3460  */}
+//       <Box bg="#656844 ">
+//         <Container p="1rem" color="white">
+//           <Box py="5rem" overflow="hidden">
+//             <Grid container spacing={6}>
+//               <Grid item lg={2} md={3} sm={6} xs={12}>
+//                 <Typography
+//                   mb="1.25rem"
+//                   lineHeight="1"
+//                   fontSize="18px"
+//                   fontWeight="600"
+//                   style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+//                 >
+//                   Customer Services
+//                 </Typography>
+//                 <div>
+//                   {aboutLinks.map((item, ind) => (
+//                     <StyledLink href="/" key={ind}>
+//                       {item}
+//                     </StyledLink>
+//                   ))}
+//                 </div>
+//               </Grid>
+
+//               <Grid item lg={2} md={3} sm={6} xs={12}>
+//                 <Typography
+//                   mb="1.25rem"
+//                   lineHeight="1"
+//                   fontSize="18px"
+//                   fontWeight="600"
+//                   style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+//                 >
+//                   Tizaraa
+//                 </Typography>
+//                 <div>
+//                   {customerCareLinks.map((item, ind) => (
+//                     <StyledLink href="/" key={ind}>
+//                       {item}
+//                     </StyledLink>
+//                   ))}
+//                 </div>
+//               </Grid>
+
+//               <Grid item lg={2} md={3} sm={6} xs={12}>
+//                 <Typography
+//                   mb="1.25rem"
+//                   lineHeight="1"
+//                   fontSize="18px"
+//                   fontWeight="600"
+//                   style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+//                 >
+//                   Earn With Tizaraa
+//                 </Typography>
+//                 <div>
+//                   {customerCareLink.map((item, ind) => (
+//                     <StyledLink href="/" key={ind}>
+//                       {item}
+//                     </StyledLink>
+//                   ))}
+//                 </div>
+//               </Grid>
+
+//               <Grid item lg={2} md={3} sm={6} xs={12}>
+//                 <Typography
+//                   mb="1.25rem"
+//                   lineHeight="1"
+//                   fontSize="18px"
+//                   fontWeight="600"
+//                   style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+//                 >
+//                   Trade services
+//                 </Typography>
+
+//                 <Typography py="0.3rem" color="gray.500">
+//                   House No: 15A, Road: 35, Gulshan 2, Dhaka 1212, Bangladesh
+//                 </Typography>
+
+
+//                 <Typography py="0.3rem" color="gray.500">
+//                   <a href="mailto:tizaraabd2023@gmail.com" style={{ textDecoration: 'none', color: 'inherit' }}>
+//                   Email: tizaraabd2023@gmail.com
+//                   </a>
+                 
+//                 </Typography>
+
+//                 <Typography py="0.3rem" mb="1rem" color="gray.500">
+//                   <a href="tel:+8801792223444" style={{ textDecoration: 'none', color: 'inherit' }}>
+//                   Phone: +8801792223444
+//                   </a>
+                 
+//                 </Typography>
+
+//                 <FlexBox className="flex" mx="-5px">
+//                   {iconList.map((item) => (
+//                     <a
+//                       href={item.url}
+//                       target="_blank"
+//                       key={item.iconName}
+//                       rel="noreferrer noopenner"
+//                     >
+//                       <Box
+//                         m="5px"
+//                         p="10px"
+//                         size="small"
+//                         borderRadius="50%"
+//                         bg="rgba(0,0,0,0.2)"
+//                       >
+//                         <Icon size="12px" defaultcolor="auto">
+//                           {item.iconName}
+//                         </Icon>
+//                       </Box>
+//                     </a>
+//                   ))}
+//                 </FlexBox>
+//               </Grid>
+
+//               <Grid item lg={4} md={12} sm={12} xs={12}>
+//                 <Link href="/">
+//                   <Image
+//                     alt="logo"
+//                     mb="1.25rem"
+//                     src="https://frontend.tizaraa.com/public/frontend/assets/img/site-logo/tizaraa-logo.png"
+//                   />
+//                 </Link>
+
+//                 <Paragraph mb="1.25rem" color="gray.500">
+//                   Bangladesh's best online shopping store with 11 thousand
+//                   products at resounding discounts in Dhaka, Chittagong &amp;
+//                   All across Bangladesh with cash on delivery (COD)
+//                 </Paragraph>
+
+//                 {/* <AppStore /> */}
+//               </Grid>
+//             </Grid>
+//           </Box>
+//         </Container>
+//       </Box>
+//     </footer>
+//   );
+// }
+
+
+import Link from "next/link";
 import Box from "@component/Box";
 import Image from "@component/Image";
 import Grid from "@component/grid/Grid";
-import Icon from "@component/icon/Icon";
 import FlexBox from "@component/FlexBox";
-import AppStore from "@component/AppStore";
 import Container from "@component/Container";
 import Typography, { Paragraph } from "@component/Typography";
 
-// STYLED COMPONENTS
-import { StyledLink } from "./styles";
 // CUSTOM DATA
-import { aboutLinks, customerCareLinks, customerCareLink, iconList } from "./data";
+export const aboutLinks = ["Terms & Conditions", "Privacy Policy"];
+export const customerCareLinks = [
+  "Help Center",
+  "Track Your Order",
+  "Corporate & Bulk Purchasing",
+  "Returns & Refunds",
+];
+export const customerCareLink = ["Sell on Tizaraa"];
+export const iconList = [
+  {
+    iconName: "YouTube",
+    url: "https://www.youtube.com/@Tizaraa24",
+    imgSrc: "/youtube.png",
+  },
+  {
+    iconName: "Instagram",
+    url: "https://www.instagram.com/tizaraagroup/?igsh=MXB1b2l1NXN6N2U5dQ%3D%3D#",
+    imgSrc: "/instagram.png",
+  },
+  // {
+  //   iconName: "LinkedIn",
+  //   url: "https://www.linkedin.com/",
+  //   imgSrc: "/facebook_seller.png",
+  // },
+  {
+    iconName: "LinkedIn",
+    url: "https://www.facebook.com/groups/1710663079506281/?ref=share&mibextid=NSMWBT",
+    imgSrc: "/facebook_seller.png",
+  },
+  {
+    iconName: "Facebook",
+    url: "https://www.facebook.com/TizaraaGroup",
+    imgSrc: "/facebook.png",
+  },
+];
 
 export default function Footer1() {
   return (
     <footer>
-      {/* #0F3460  */}
-      <Box bg="#656844 ">
-        <Container p="1rem" color="white">
-          <Box py="5rem" overflow="hidden">
+      <Box style={{ backgroundColor: "#656844" }}>
+        <Container style={{ padding: "1rem", color: "white" }}>
+          <Box style={{ padding: "5rem 0", overflow: "hidden" }}>
             <Grid container spacing={6}>
+              {/* Customer Services */}
               <Grid item lg={2} md={3} sm={6} xs={12}>
                 <Typography
-                  mb="1.25rem"
-                  lineHeight="1"
-                  fontSize="18px"
-                  fontWeight="600"
-                  style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+                  style={{
+                    marginBottom: "1.25rem",
+                    lineHeight: "1",
+                    fontSize: "18px",
+                    fontWeight: "600",
+                    whiteSpace: "nowrap",
+                  }}
                 >
                   Customer Services
                 </Typography>
                 <div>
                   {aboutLinks.map((item, ind) => (
-                    <StyledLink href="/" key={ind}>
+                    <a
+                      href="/"
+                      key={ind}
+                      style={{
+                        display: "block",
+                        color: "white",
+                        textDecoration: "none",
+                        marginBottom: "0.5rem",
+                      }}
+                    >
                       {item}
-                    </StyledLink>
+                    </a>
                   ))}
                 </div>
               </Grid>
 
+              {/* Tizaraa */}
               <Grid item lg={2} md={3} sm={6} xs={12}>
                 <Typography
-                  mb="1.25rem"
-                  lineHeight="1"
-                  fontSize="18px"
-                  fontWeight="600"
-                  style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+                  style={{
+                    marginBottom: "1.25rem",
+                    lineHeight: "1",
+                    fontSize: "18px",
+                    fontWeight: "600",
+                    whiteSpace: "nowrap",
+                  }}
                 >
                   Tizaraa
                 </Typography>
                 <div>
                   {customerCareLinks.map((item, ind) => (
-                    <StyledLink href="/" key={ind}>
+                    <a
+                      href="/"
+                      key={ind}
+                      style={{
+                        display: "block",
+                        color: "white",
+                        textDecoration: "none",
+                        marginBottom: "0.5rem",
+                      }}
+                    >
                       {item}
-                    </StyledLink>
+                    </a>
                   ))}
                 </div>
               </Grid>
 
+              {/* Earn With Tizaraa */}
               <Grid item lg={2} md={3} sm={6} xs={12}>
                 <Typography
-                  mb="1.25rem"
-                  lineHeight="1"
-                  fontSize="18px"
-                  fontWeight="600"
-                  style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+                  style={{
+                    marginBottom: "1.25rem",
+                    lineHeight: "1",
+                    fontSize: "18px",
+                    fontWeight: "600",
+                    whiteSpace: "nowrap",
+                  }}
                 >
                   Earn With Tizaraa
                 </Typography>
                 <div>
                   {customerCareLink.map((item, ind) => (
-                    <StyledLink href="/" key={ind}>
+                    <a
+                      href="/"
+                      key={ind}
+                      style={{
+                        display: "block",
+                        color: "white",
+                        textDecoration: "none",
+                        marginBottom: "0.5rem",
+                      }}
+                    >
                       {item}
-                    </StyledLink>
+                    </a>
                   ))}
                 </div>
               </Grid>
 
+              {/* Contact Info */}
               <Grid item lg={2} md={3} sm={6} xs={12}>
                 <Typography
-                  mb="1.25rem"
-                  lineHeight="1"
-                  fontSize="18px"
-                  fontWeight="600"
-                  style={{ whiteSpace: "nowrap" }} // Prevents breaking into multiple lines
+                  style={{
+                    marginBottom: "1.25rem",
+                    lineHeight: "1",
+                    fontSize: "18px",
+                    fontWeight: "600",
+                    whiteSpace: "nowrap",
+                  }}
                 >
                   Trade services
                 </Typography>
-
-                <Typography py="0.3rem" color="gray.500">
-                  House No: 15A, Road: 35, Gulshan 2, Dhaka 1212, Bangladesh
+                <Typography style={{ padding: "0.3rem 0", color: "gray" }}>
+                  House No: 15A, Road: 35, Gulshan 2, Dhaka 1212, Bangladesh
                 </Typography>
-
-
-                <Typography py="0.3rem" color="gray.500">
-                  <a href="mailto:tizaraabd2023@gmail.com" style={{ textDecoration: 'none', color: 'inherit' }}>
-                  Email: tizaraabd2023@gmail.com
+                <Typography style={{ padding: "0.3rem 0", color: "gray" }}>
+                  <a
+                    href="mailto:tizaraabd2023@gmail.com"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Email: tizaraabd2023@gmail.com
                   </a>
-                 
                 </Typography>
-
-                <Typography py="0.3rem" mb="1rem" color="gray.500">
-                  <a href="tel:+8801792223444" style={{ textDecoration: 'none', color: 'inherit' }}>
-                  Phone: +8801792223444
+                <Typography
+                  style={{
+                    padding: "0.3rem 0",
+                    marginBottom: "1rem",
+                    color: "gray",
+                  }}
+                >
+                  <a
+                    href="tel:+8801792223444"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Phone: +8801792223444
                   </a>
-                 
                 </Typography>
 
-                <FlexBox className="flex" mx="-5px">
-                  {iconList.map((item) => (
+                <FlexBox style={{ display: "flex", gap: "5px" }}>
+                  {iconList.map((item, ind) => (
                     <a
                       href={item.url}
                       target="_blank"
-                      key={item.iconName}
-                      rel="noreferrer noopenner"
+                      rel="noopener noreferrer"
+                      key={ind}
                     >
                       <Box
-                        m="5px"
-                        p="10px"
-                        size="small"
-                        borderRadius="50%"
-                        bg="rgba(0,0,0,0.2)"
+                        style={{
+                          margin: "0px",
+                          paddingTop: "8px",
+                          paddingLeft: "0px",
+                          paddingBottom: "8px",
+                          paddingRight: "10px",
+                          borderRadius: "50%",
+                          //backgroundColor: "rgba(0,0,0,0.2)",
+                        }}
                       >
-                        <Icon size="12px" defaultcolor="auto">
-                          {item.iconName}
-                        </Icon>
+                        <img
+                          src={item.imgSrc}
+                          alt={item.iconName}
+                          style={{
+                            width: "24px",
+                            height: "24px",
+                            display: "block",
+                          }}
+                        />
                       </Box>
                     </a>
                   ))}
                 </FlexBox>
               </Grid>
 
+              {/* Logo and Description */}
               <Grid item lg={4} md={12} sm={12} xs={12}>
                 <Link href="/">
                   <Image
                     alt="logo"
-                    mb="1.25rem"
+                    style={{ marginBottom: "1.25rem", maxWidth: "150px" }}
                     src="https://frontend.tizaraa.com/public/frontend/assets/img/site-logo/tizaraa-logo.png"
                   />
                 </Link>
-
-                <Paragraph mb="1.25rem" color="gray.500">
+                <Paragraph style={{ marginBottom: "1.25rem", color: "gray" }}>
                   Bangladesh's best online shopping store with 11 thousand
                   products at resounding discounts in Dhaka, Chittagong &amp;
                   All across Bangladesh with cash on delivery (COD)
                 </Paragraph>
-
-                {/* <AppStore /> */}
               </Grid>
             </Grid>
           </Box>
@@ -157,3 +399,4 @@ export default function Footer1() {
     </footer>
   );
 }
+
