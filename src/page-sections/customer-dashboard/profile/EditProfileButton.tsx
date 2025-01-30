0dccf47ed0c71bcabd0eb50910b40ce3025380de
Nahida Zaman Bina <nahidazmn@gmail.com>
2025-01-30 15:32:24 +0600

    update edit profile component

--- src/page-sections/customer-dashboard/profile/EditProfileButton.tsx
+++ src/page-sections/customer-dashboard/profile/EditProfileButton.tsx
@@ -1,3 +1,27 @@
+// "use client";
+
+// import { useRouter } from "next/navigation";
+// import { Button } from "@component/buttons";
+// import FlexBox from "@component/FlexBox";
+
+// export default function EditProfileButton() {
+//   const { push } = useRouter();
+
+//   return (
+//     <FlexBox style={{gap:"1rem"}}>
+//     <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/edit")}>
+//       Edit Profile
+//     </Button>
+
+//     <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/set-password")}>
+//       Change Password
+//     </Button>
+    
+//     </FlexBox>
+//   );
+// }
+
+
 "use client";
 
 import { useRouter } from "next/navigation";
@@ -7,16 +31,22 @@ import FlexBox from "@component/FlexBox";
 export default function EditProfileButton() {
   const { push } = useRouter();
 
-  return (
-    <FlexBox style={{gap:"1rem"}}>
-    <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/edit")}>
-      Edit Profile
-    </Button>
+  // Retrieve userInfo from localStorage
+  const userInfo = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("userInfo")) : null;
+  const status = userInfo?.status;
 
-    <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/set-password")}>
-      Change Password
-    </Button>
-    
+  return (
+    <FlexBox style={{ gap: "1rem" }}>
+      <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/edit")}>
+        Edit Profile
+      </Button>
+
+      
+      {status !== 2 && (
+        <Button color="primary" bg="primary.light" px="2rem" onClick={() => push("/profile/set-password")}>
+          Change Password
+        </Button>
+      )}
     </FlexBox>
   );
 }
