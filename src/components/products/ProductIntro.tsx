115181c98df60e426dd8acda817d3b0d6223b9e3
Nahida Zaman Bina <nahidazmn@gmail.com>
2025-03-12 14:44:50 +0600

    add to cart issue solve

--- src/components/products/ProductIntro.tsx
+++ src/components/products/ProductIntro.tsx
@@ -119,6 +119,7 @@ export default function ProductIntro({
                      color: "white",
                      borderRadius: "5px",
                      border: "none",
+                     cursor: "pointer"
                    }}
                  >
                    Request for Quote
