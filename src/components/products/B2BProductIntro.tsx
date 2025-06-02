916ca6786de62decd03802c56172e11584eea40d
Mahmudul Noman <mhnoman75@gmail.com>
2025-06-02 15:33:52 +0600

    Design Min Quantity

--- src/components/products/B2BProductIntro.tsx
+++ src/components/products/B2BProductIntro.tsx
@@ -1,4 +1,4 @@
-// "use client"
+// "use client";
 // import { useParams } from "next/navigation";
 // import Box from "@component/Box";
 // import Grid from "@component/grid/Grid";
@@ -6,14 +6,27 @@
 // import { useState } from "react";
 // import ProductImages from "./ProductImages";
 // import AddToCartButton from "./AddToCartButton";
-// import ProductDetails from "./ProductDetails";
-// import Link from "next/link";
 // import B2BProductDetails from "./B2BProductDetails";
+// import Link from "next/link";
+
+// type B2BProductSize = {
+//   id: number;
+//   stock_quantity: number;
+//   base_price: number;
+//   options: {
+//     color: string | null;
+//     size: string;
+//   };
+//   bulk_pricing: {
+//     min_quantity: string;
+//     unit_price: number;
+//   }[];
+// };
 
 // type ProductIntroProps = {
 //   price: number;
-//   discountPrice?: number; 
-//   totalDiscount?: number; 
+//   discountPrice?: number;
+//   totalDiscount?: number;
 //   title: string;
 //   images: string[];
 //   id: string | number;
@@ -21,18 +34,22 @@
 //   sellerShopLogo: string;
 //   brandName: string;
 //   warranty: string;
-//   warrantyType: string,
-//   replacewarranty: string,
+//   warrantyType: string;
+//   replacewarranty: string;
 //   rating: number;
 //   productStock: number;
 //   slug?: string;
 //   productId: string | number;
 //   sellerId: string | number;
 //   sizeColor?: {
-//     colorwithsize: {
+//     colorwithsize?: {
 //       [color: string]: { size: string; price: string; qty: string }[];
 //     };
+//     size?: { size: string; price: string; qty: string }[];
+//     color?: { color: string; price: string; qty: string }[];
 //   };
+//   b2bProductSize?: B2BProductSize[];
+//   delivereyType?: string;
 // };
 
 // export default function ProductIntro({
@@ -54,6 +71,8 @@
 //   productId,
 //   sellerId,
 //   sizeColor,
+//   b2bProductSize,
+//   delivereyType,
 // }: ProductIntroProps) {
 //   const param = useParams();
 //   const { state } = useAppContext();
@@ -62,15 +81,40 @@
 //   const [selectedColor, setSelectedColor] = useState<string | null>(null);
 //   const [selectedSize, setSelectedSize] = useState<string | null>(null);
 //   const [selectedPrice, setSelectedPrice] = useState<number>(price);
+//   const [selectedB2BQuantity, setSelectedB2BQuantity] = useState<number>(1);
 
-//   const handleSelectionChange = (color: string | null, size: string | null, updatedPrice: number) => {
+//   const handleSelectionChange = (
+//     color: string | null,
+//     size: string | null,
+//     updatedPrice: number,
+//     selectedQuantity?: number
+//   ) => {
 //     setSelectedColor(color);
 //     setSelectedSize(size);
 //     setSelectedPrice(updatedPrice);
+//     if (selectedQuantity !== undefined) {
+//       setSelectedB2BQuantity(selectedQuantity);
+//     }
+//   };
+
+//   const styles = {
+//     rfqButton: {
+//       padding: "10px 20px",
+//       backgroundColor: "#E94560",
+//       color: "#ffffff",
+//       borderRadius: "5px",
+//       border: "none",
+//       cursor: "pointer",
+//       fontSize: "16px",
+//       fontWeight: "500",
+//     },
+//     rfqContainer: {
+//       marginTop: "16px",
+//     },
 //   };
 
 //   return (
-//     <Box overflow="hidden">
+//     <Box style={{ overflow: "hidden" }}>
 //       <Grid container justifyContent="center" spacing={16}>
 //         <Grid item md={6} xs={12} alignItems="center">
 //           <ProductImages images={images} />
@@ -91,47 +135,36 @@
 //             warrantyType={warrantyType}
 //             replacewarranty={replacewarranty}
 //             sizeColor={sizeColor}
+//             b2bProductSize={b2bProductSize}
 //             onSelectionChange={handleSelectionChange}
+//             delivereyType={delivereyType}
 //           />
-         
 
-//             {selectedPrice === 0 ? (
-//              <Box mt="1rem">
-//                <Link href="/rfq">
-//                  <button
-//                    style={{
-//                      padding: "10px 20px",
-//                      backgroundColor: "#E94560",
-//                      color: "white",
-//                      borderRadius: "5px",
-//                      border: "none",
-//                      cursor: "pointer"
-//                    }}
-//                  >
-//                    Request for Quote
-//                  </button>
-//                </Link>
-//              </Box>
-//            ) : (
-//              <AddToCartButton
-//              productId={productId}
-//              variantId={""}
-//              sellerId={sellerId}
-//              images={images}
-//              title={title}
-//              discountPrice={discountPrice}
-//              slug={slug}
-//              productStock={productStock}
-//              price={price} 
-//              productType="General"
-//              sizeColor={sizeColor}
-//              selectedColor={selectedColor}
-//              selectedSize={selectedSize}
-//              selectedPrice={selectedPrice}
- 
-//            />
-//            )}
- 
+//           {selectedPrice === 0 ? (
+//             <Box style={styles.rfqContainer}>
+//               <Link href="/rfq">
+//                 <button style={styles.rfqButton}>Request for Quote</button>
+//               </Link>
+//             </Box>
+//           ) : (
+//             <AddToCartButton
+//               productId={productId}
+//               variantId={""}
+//               sellerId={sellerId}
+//               images={images}
+//               title={title}
+//               discountPrice={discountPrice}
+//               slug={slug}
+//               productStock={productStock}
+//               price={selectedPrice}
+//               productType="General"
+//               sizeColor={sizeColor}
+//               selectedColor={selectedColor}
+//               selectedSize={selectedSize}
+//               selectedPrice={selectedPrice}
+//               // selectedB2BQuantity={selectedB2BQuantity}
+//             />
+//           )}
 //         </Grid>
 //       </Grid>
 //     </Box>
@@ -139,6 +172,182 @@
 // }
 
 
+
+
+
+
+// "use client";
+// import { useParams } from "next/navigation";
+// import Box from "@component/Box";
+// import Grid from "@component/grid/Grid";
+// import { useAppContext } from "@context/app-context";
+// import { useState } from "react";
+// import ProductImages from "./ProductImages";
+// import AddToCartButton from "./AddToCartButton";
+// import B2BProductDetails from "./B2BProductDetails";
+// import Link from "next/link";
+
+// type B2BProductSize = {
+//   id: number;
+//   stock_quantity: number;
+//   base_price: number;
+//   options: {
+//     color: string | null;
+//     size: string;
+//   };
+//   bulk_pricing: {
+//     min_quantity: string;
+//     unit_price: number;
+//   }[];
+// };
+
+// type ProductIntroProps = {
+//   price: number;
+//   discountPrice?: number;
+//   totalDiscount?: number;
+//   title: string;
+//   images: string[];
+//   id: string | number;
+//   sellerShopName: string;
+//   sellerShopLogo: string;
+//   brandName: string;
+//   warranty: string;
+//   warrantyType: string;
+//   replacewarranty: string;
+//   rating: number;
+//   productStock: number;
+//   slug?: string;
+//   productId: string | number;
+//   sellerId: string | number;
+//   sizeColor?: {
+//     colorwithsize?: {
+//       [color: string]: { size: string; price: string; qty: string }[];
+//     };
+//     size?: { size: string; price: string; qty: string }[];
+//     color?: { color: string; price: string; qty: string }[];
+//   };
+//   b2bProductSize?: B2BProductSize[];
+//   delivereyType?: string;
+// };
+
+// export default function ProductIntro({
+//   images,
+//   title,
+//   price,
+//   id,
+//   sellerShopName,
+//   sellerShopLogo,
+//   brandName,
+//   warranty,
+//   warrantyType,
+//   replacewarranty,
+//   rating,
+//   discountPrice,
+//   totalDiscount,
+//   slug,
+//   productStock,
+//   productId,
+//   sellerId,
+//   sizeColor,
+//   b2bProductSize,
+//   delivereyType,
+// }: ProductIntroProps) {
+//   const param = useParams();
+//   const { state } = useAppContext();
+//   const routerId = param.slug as string;
+
+//   const [selectedColor, setSelectedColor] = useState<string | null>(null);
+//   const [selectedSize, setSelectedSize] = useState<string | null>(null);
+//   const [selectedPrice, setSelectedPrice] = useState<number>(price);
+//   const [selectedB2BQuantity, setSelectedB2BQuantity] = useState<number>(1);
+
+//   const handleSelectionChange = (
+//     color: string | null,
+//     size: string | null,
+//     updatedPrice: number,
+//     selectedQuantity?: number
+//   ) => {
+//     setSelectedColor(color);
+//     setSelectedSize(size);
+//     setSelectedPrice(updatedPrice);
+//     if (selectedQuantity !== undefined) {
+//       setSelectedB2BQuantity(selectedQuantity);
+//     }
+//   };
+
+//   const styles = {
+//     rfqButton: {
+//       padding: "10px 20px",
+//       backgroundColor: "#E94560",
+//       color: "#ffffff",
+//       borderRadius: "5px",
+//       border: "none",
+//       cursor: "pointer",
+//       fontSize: "16px",
+//       fontWeight: "500",
+//     },
+//     rfqContainer: {
+//       marginTop: "16px",
+//     },
+//   };
+
+//   return (
+//     <Box style={{ overflow: "hidden" }}>
+//       <Grid container justifyContent="center" spacing={16}>
+//         <Grid item md={6} xs={12} alignItems="center">
+//           <ProductImages images={images} />
+//         </Grid>
+
+//         <Grid item md={6} xs={12} alignItems="center">
+//           <B2BProductDetails
+//             title={title}
+//             price={price}
+//             discountPrice={discountPrice}
+//             totalDiscount={totalDiscount}
+//             rating={rating}
+//             productStock={productStock}
+//             sellerShopName={sellerShopName}
+//             sellerShopLogo={sellerShopLogo}
+//             brandName={brandName}
+//             warranty={warranty}
+//             warrantyType={warrantyType}
+//             replacewarranty={replacewarranty}
+//             sizeColor={sizeColor}
+//             b2bProductSize={b2bProductSize}
+//             onSelectionChange={handleSelectionChange}
+//             delivereyType={delivereyType}
+//           />
+
+//           {selectedPrice === 0 ? (
+//             <Box style={styles.rfqContainer}>
+//               <Link href="/rfq">
+//                 <button style={styles.rfqButton}>Request for Quote</button>
+//               </Link>
+//             </Box>
+//           ) : (
+//             <AddToCartButton
+//               productId={productId}
+//               variantId={""}
+//               sellerId={sellerId}
+//               images={images}
+//               title={title}
+//               discountPrice={discountPrice}
+//               slug={slug}
+//               productStock={productStock}
+//               price={selectedPrice}
+//               productType="General"
+//               sizeColor={sizeColor}
+//               selectedColor={selectedColor}
+//               selectedSize={selectedSize}
+//               selectedPrice={selectedPrice}
+//               selectedB2BQuantity={selectedB2BQuantity}
+//             />
+//           )}
+//         </Grid>
+//       </Grid>
+//     </Box>
+//   );
+// }
 
 
 
@@ -194,6 +403,7 @@ type ProductIntroProps = {
     color?: { color: string; price: string; qty: string }[];
   };
   b2bProductSize?: B2BProductSize[];
+  delivereyType?: string;
 };
 
 export default function ProductIntro({
@@ -216,6 +426,7 @@ export default function ProductIntro({
   sellerId,
   sizeColor,
   b2bProductSize,
+  delivereyType,
 }: ProductIntroProps) {
   const param = useParams();
   const { state } = useAppContext();
@@ -225,6 +436,7 @@ export default function ProductIntro({
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
   const [selectedPrice, setSelectedPrice] = useState<number>(price);
   const [selectedB2BQuantity, setSelectedB2BQuantity] = useState<number>(1);
+  const [currentQuantity, setCurrentQuantity] = useState<number>(1);
 
   const handleSelectionChange = (
     color: string | null,
@@ -280,6 +492,8 @@ export default function ProductIntro({
             sizeColor={sizeColor}
             b2bProductSize={b2bProductSize}
             onSelectionChange={handleSelectionChange}
+            delivereyType={delivereyType}
+            currentQuantity={currentQuantity}
           />
 
           {selectedPrice === 0 ? (
@@ -304,7 +518,8 @@ export default function ProductIntro({
               selectedColor={selectedColor}
               selectedSize={selectedSize}
               selectedPrice={selectedPrice}
-              selectedB2BQuantity={selectedB2BQuantity}
+              currentQuantity={currentQuantity}
+              setCurrentQuantity={setCurrentQuantity}
             />
           )}
         </Grid>
@@ -312,19 +527,3 @@ export default function ProductIntro({
     </Box>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
