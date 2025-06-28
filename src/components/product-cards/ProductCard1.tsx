79f1d68fd2a3d104492f4063874b109d526a6424
Mahmudul Noman <mhnoman75@gmail.com>
2025-06-28 13:42:52 +0600

    Product Cad mobile responsive

--- src/components/product-cards/ProductCard1.tsx
+++ src/components/product-cards/ProductCard1.tsx
@@ -18,7 +18,12 @@
 // import ProductQuickView from "@component/products/ProductQuickView";
 // import { toast } from "react-hot-toast";
 
-// import { calculateDiscount, currency, getTheme, DiscountPercentage } from "@utils/utils";
+// import {
+//   calculateDiscount,
+//   currency,
+//   getTheme,
+//   DiscountPercentage,
+// } from "@utils/utils";
 // import { deviceSize } from "@utils/constants";
 
 // // STYLED COMPONENT
@@ -30,6 +35,10 @@
 //   flex-direction: column;
 //   justify-content: space-between;
 //   transition: all 250ms ease-in-out;
+
+//   @media only screen and (max-width: ${deviceSize.sm}px) {
+//     height: auto; // removes fixed height on mobile
+//   }
 
 //   &:hover {
 //     .details {
@@ -61,17 +70,48 @@
 
 //     @media only screen and (max-width: ${deviceSize.sm}px) {
 //       display: block;
+
+//       img {
+//         width: 150px !important; /* or your desired smaller width */
+//         height: auto !important;
+//       }
 //     }
 //   }
 
 //   .details {
 //     padding: 1rem;
 
+//     @media only screen and (max-width: ${deviceSize.sm}px) {
+//       padding: 0.5rem;
+//     }
+
 //     .title,
 //     .categories {
 //       overflow: hidden;
 //       white-space: nowrap;
 //       text-overflow: ellipsis;
+//     }
+
+//     .title {
+//       font-size: 14px;
+
+//       @media only screen and (max-width: ${deviceSize.sm}px) {
+//         font-size: 12px;
+//         display: -webkit-box;
+//         -webkit-line-clamp: 2;
+//         -webkit-box-orient: vertical;
+//         overflow: hidden;
+//         text-overflow: ellipsis;
+//         white-space: normal;
+//       }
+//     }
+
+//     .price {
+//       font-size: 14px;
+
+//       @media only screen and (max-width: ${deviceSize.sm}px) {
+//         font-size: 13px;
+//       }
 //     }
 
 //     .icon-holder {
@@ -84,11 +124,13 @@
 //     .favorite-icon {
 //       cursor: pointer;
 //     }
+
 //     .outlined-icon {
 //       svg path {
 //         fill: ${getTheme("colors.text.hint")};
 //       }
 //     }
+
 //     .add-cart {
 //       display: none;
 //       margin-top: auto;
@@ -121,7 +163,7 @@
 //   id?: string | number;
 //   productId?: string | number;
 //   sellerId?: string | number;
-//   sizecolorwithprice?: any; 
+//   sizecolorwithprice?: any;
 // }
 // // =======================================================================
 
@@ -143,7 +185,7 @@
 // }: ProductCard1Props) {
 //   const [open, setOpen] = useState(false);
 //   const { state, dispatch } = useAppContext();
-  
+
 //   const cartItem = state.cart.find((item) => item.id === id);
 
 //   const toggleDialog = useCallback(() => setOpen((open) => !open), []);
@@ -164,12 +206,12 @@
 //         name: title,
 //         qty: amount,
 
-//           // newly added 
-//           discountPrice,   
-//           productId,
-//           sellerId,
-//           sizecolorwithprice 
-//       }
+//         // newly added
+//         discountPrice,
+//         productId,
+//         sellerId,
+//         sizecolorwithprice,
+//       },
 //     });
 //   };
 
@@ -177,7 +219,6 @@
 //     <>
 //       <Wrapper borderRadius={8} {...props}>
 //         <div className="image-holder">
-
 //           {!!off && (
 //             <Chip
 //               top="10px"
@@ -188,7 +229,8 @@
 //               bg="primary.main"
 //               position="absolute"
 //               color="primary.text"
-//               zIndex={1}>
+//               zIndex={1}
+//             >
 //               {/* {off}%  */}
 //               {DiscountPercentage(price, off as number)}% off
 //             </Chip>
@@ -220,18 +262,17 @@
 //                   textAlign="left"
 //                   fontWeight="600"
 //                   className="title"
-//                   color="text.secondary">
+//                   color="text.secondary"
+//                 >
 //                   {title}
 //                 </H3>
 //               </Link>
 
-                         
-//             {rating > 0 && (
-//   <Rating value={rating} outof={5} color="warn" readOnly />
-// )}
+//               {rating > 0 && (
+//                 <Rating value={rating} outof={5} color="warn" readOnly />
+//               )}
 
-
-// {/* {(price === 0 && off === 0) ? (
+//               {/* {(price === 0 && off === 0) ? (
 //     <H4 fontWeight="600" fontSize="14px" color="primary.main">
 //         RFQ
 //     </H4>
@@ -259,30 +300,38 @@
 //     </>
 // )} */}
 
-// {(price === 0 && off === 0) ? (
-//     <H4 fontWeight="600" fontSize="14px" color="primary.main">
-//         RFQ
-//     </H4>
-// ) : (
-//   <FlexBox mt="0.5rem" mb="1rem" alignItems="center">
-//   <H5 fontWeight={600} color="primary.main" mr="0.5rem">
-//     {calculateDiscount(price, off as number)}
-//   </H5>
+//               {price === 0 && off === 0 ? (
+//                 <H4 fontWeight="600" fontSize="14px" color="primary.main">
+//                   RFQ
+//                 </H4>
+//               ) : (
+//                 <FlexBox
+//                   mt="0.5rem"
+//                   mb="1rem"
+//                   alignItems="flex-start"
+//                   flexDirection="column"
+//                 >
+//                   <H5
+//                     className="price"
+//                     fontWeight={600}
+//                     color="primary.main"
+//                     mb="0.25rem"
+//                   >
+//                     {calculateDiscount(price, off as number)}
+//                   </H5>
 
-//   {off > 0 && (
-// <SemiSpan fontWeight="600">
-// <del>{currency(price)}</del>
-// </SemiSpan>
-// )}
-
-// </FlexBox>
-// )}
-
-    
+//                   {off > 0 && (
+//                     <SemiSpan
+//                       className="price"
+//                       fontWeight="600"
+//                       color="text.muted"
+//                     >
+//                       <del>{currency(price)}</del>
+//                     </SemiSpan>
+//                   )}
+//                 </FlexBox>
+//               )}
 //             </Box>
-
-          
-            
 
 //             {/* <FlexBox
 //               width="30px"
@@ -334,24 +383,32 @@
 
 
 
-
 "use client";
 
 import Link from "next/link";
-import { useCallback, useState } from "react";
+import { Fragment, useCallback, useState } from "react";
 import styled from "styled-components";
 
 import { useAppContext } from "@context/app-context";
+
 import Box from "@component/Box";
+import Rating from "@component/rating";
 import { Chip } from "@component/Chip";
+import Icon from "@component/icon/Icon";
 import FlexBox from "@component/FlexBox";
-import { H3, H5, SemiSpan } from "@component/Typography";
+import { Button } from "@component/buttons";
 import NextImage from "@component/NextImage";
 import Card, { CardProps } from "@component/Card";
+import { H3, H4, H5, SemiSpan } from "@component/Typography";
 import ProductQuickView from "@component/products/ProductQuickView";
 import { toast } from "react-hot-toast";
 
-import { calculateDiscount, currency, DiscountPercentage } from "@utils/utils";
+import {
+  calculateDiscount,
+  currency,
+  getTheme,
+  DiscountPercentage,
+} from "@utils/utils";
 import { deviceSize } from "@utils/constants";
 
 // STYLED COMPONENT
@@ -364,12 +421,20 @@ const Wrapper = styled(Card)`
   justify-content: space-between;
   transition: all 250ms ease-in-out;
 
+  @media only screen and (max-width: ${deviceSize.sm}px) {
+    height: 280px; // removes fixed height on mobile
+  }
+
   &:hover {
-    .details .add-cart {
-      display: flex;
+    .details {
+      .add-cart {
+        display: flex;
+      }
     }
-    .image-holder .extra-icons {
-      display: block;
+    .image-holder {
+      .extra-icons {
+        display: block;
+      }
     }
   }
 
@@ -378,15 +443,6 @@ const Wrapper = styled(Card)`
     position: relative;
     display: inline-block;
     height: 100%;
-
-    img {
-      width: 100%;
-      height: auto;
-      max-width: 140px;
-      @media only screen and (max-width: ${deviceSize.sm}px) {
-        max-width: 100px;
-      }
-    }
 
     .extra-icons {
       z-index: 2;
@@ -399,43 +455,85 @@ const Wrapper = styled(Card)`
 
     @media only screen and (max-width: ${deviceSize.sm}px) {
       display: block;
+
+      img {
+        width: 150px !important; /* or your desired smaller width */
+        height: auto !important;
+      }
     }
   }
 
   .details {
     padding: 1rem;
 
-    .title {
-      font-size: 14px;
-      font-weight: 600;
-      color: ${({ theme }) => theme.colors.text.secondary};
-
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
-      text-overflow: ellipsis;
-
-      @media only screen and (max-width: ${deviceSize.sm}px) {
-        font-size: 12px;
-      }
-    }
-
     @media only screen and (max-width: ${deviceSize.sm}px) {
       padding: 0.5rem;
     }
+
+    .title,
+    .categories {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+
+    .title {
+      font-size: 14px;
+
+      @media only screen and (max-width: ${deviceSize.sm}px) {
+        font-size: 12px;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: normal;
+      }
+    }
+
+    .price {
+      font-size: 14px;
+
+      @media only screen and (max-width: ${deviceSize.sm}px) {
+        font-size: 13px;
+      }
+    }
+
+    .icon-holder {
+      display: flex;
+      align-items: flex-end;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .favorite-icon {
+      cursor: pointer;
+    }
+
+    .outlined-icon {
+      svg path {
+        fill: ${getTheme("colors.text.hint")};
+      }
+    }
+
+    .add-cart {
+      display: none;
+      margin-top: auto;
+      align-items: center;
+      flex-direction: column;
+    }
   }
 
-  .chip {
-    font-size: 10px;
-
-    @media only screen and (max-width: ${deviceSize.sm}px) {
-      font-size: 8px;
-      padding: 2px 6px;
+  @media only screen and (max-width: 768px) {
+    .details {
+      .add-cart {
+        display: flex;
+      }
     }
   }
 `;
 
+// =======================================================================
 interface ProductCard1Props extends CardProps {
   off?: number;
   slug: string;
@@ -445,13 +543,14 @@ interface ProductCard1Props extends CardProps {
   rating: number;
   images: string[];
   productStock: number;
-  discountPrice?: number;
-  totalDiscount?: number;
+  discountPrice?: number; // Optional discount price
+  totalDiscount?: number; // Optional total discount
   id?: string | number;
   productId?: string | number;
   sellerId?: string | number;
   sizecolorwithprice?: any;
 }
+// =======================================================================
 
 export default function ProductCard1({
   id,
@@ -473,6 +572,7 @@ export default function ProductCard1({
   const { state, dispatch } = useAppContext();
 
   const cartItem = state.cart.find((item) => item.id === id);
+
   const toggleDialog = useCallback(() => setOpen((open) => !open), []);
 
   const handleCartAmountChange = (amount: number) => () => {
@@ -480,7 +580,6 @@ export default function ProductCard1({
       toast.error("Out of Stock");
       return;
     }
-
     dispatch({
       type: "CHANGE_CART_AMOUNT",
       payload: {
@@ -491,6 +590,8 @@ export default function ProductCard1({
         productStock,
         name: title,
         qty: amount,
+
+        // newly added
         discountPrice,
         productId,
         sellerId,
@@ -505,7 +606,6 @@ export default function ProductCard1({
         <div className="image-holder">
           {!!off && (
             <Chip
-              className="chip"
               top="10px"
               left="10px"
               p="5px 10px"
@@ -516,38 +616,70 @@ export default function ProductCard1({
               color="primary.text"
               zIndex={1}
             >
+              {/* {off}%  */}
               {DiscountPercentage(price, off as number)}% off
             </Chip>
           )}
 
           <Link href={`/product/${slug}`}>
-            <NextImage
-              alt={title}
-              width={140}
-              height={140}
-              src={imgUrl}
-              style={{ width: "100px", height: "auto", maxWidth: "100%" }}
-            />
+            <NextImage alt={title} width={277} src={imgUrl} height={270} />
           </Link>
         </div>
 
         <div className="details">
-          <Link href={`/product/${slug}`}>
-            <H3 className="title" title={title}>
-              {title}
-            </H3>
-          </Link>
+          <FlexBox>
+            <Box flex="1 1 0" minWidth="0px" mr="0.5rem">
+              <Link href={`/product/${slug}`}>
+                <H3
+                  mb="10px"
+                  title={title}
+                  fontSize="14px"
+                  textAlign="left"
+                  fontWeight="600"
+                  className="title"
+                  color="text.secondary"
+                >
+                  {title}
+                </H3>
+              </Link>
 
-          <Box mt="0.3rem">
-            <H5 fontWeight={600} fontSize="12px" color="primary.main">
-              {calculateDiscount(price, off as number)}
-            </H5>
-            {off > 0 && (
-              <SemiSpan fontWeight="600" fontSize="10px" color="text.muted">
-                <del>{currency(price)}</del>
-              </SemiSpan>
-            )}
-          </Box>
+              {rating > 0 && (
+                <Rating value={rating} outof={5} color="warn" readOnly />
+              )}
+
+              {price === 0 && off === 0 ? (
+                <H4 fontWeight="600" fontSize="14px" color="primary.main">
+                  RFQ
+                </H4>
+              ) : (
+                <FlexBox
+                  mt="0.5rem"
+                  mb="1rem"
+                  alignItems="flex-start"
+                  flexDirection="column"
+                >
+                  <H5
+                    className="price"
+                    fontWeight={600}
+                    color="primary.main"
+                    mb="0.25rem"
+                  >
+                    {calculateDiscount(price, off as number)}
+                  </H5>
+
+                  {off > 0 && (
+                    <SemiSpan
+                      className="price"
+                      fontWeight="600"
+                      color="text.muted"
+                    >
+                      <del>{currency(price)}</del>
+                    </SemiSpan>
+                  )}
+                </FlexBox>
+              )}
+            </Box>
+          </FlexBox>
         </div>
       </Wrapper>
 
