521cd8d0f1492e5a4d6e7b78836af6d281b8a1a4
Mahmudul Noman <mhnoman75@gmail.com>
2025-07-19 12:13:09 +0600

    OTC image

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     theme: "DEFAULT",
   },
   images: {
-    domains: ['seller.tizaraa.com', 'ae01.alicdn.com'],  // Add the domain here
+    domains: ['seller.tizaraa.com', 'ae01.alicdn.com','cbu01.alicdn.com'],  // Add the domain here
     unoptimized: true,
   },
   experimental: {
