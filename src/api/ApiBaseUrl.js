1ffd22f9ad9e36593575d220a39c20de7a1102ac
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2025-09-09 16:57:57 +0600

    uat

--- src/api/ApiBaseUrl.js
+++ src/api/ApiBaseUrl.js
@@ -1,14 +1,14 @@
 const ApiBaseUrl = 
     {
     // Live
-       baseUrl1: `https://frontend.tizaraa.com/api/`,
-       baseUrl:`https://frontend.tizaraa.shop/api/`,
-       ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
+    //    baseUrl1: `https://frontend.tizaraa.com/api/`,
+    //    baseUrl:`https://frontend.tizaraa.shop/api/`,
+    //    ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
 
     // UAT
-    //    baseUrl1: `https://frontend.tizaraa.com/api/`,
-    //    baseUrl:`https://uat-client.tizaraa.shop/api/`,
-    //    ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
+       baseUrl1: `https://frontend.tizaraa.com/api/`,
+       baseUrl:`https://uat-client.tizaraa.shop/api/`,
+       ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
     }
 
 
