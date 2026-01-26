fa87aea12cc6eab001ec42e9cfcbde6107d0072d
OmorFaruk63 <omor3710@gmail.com>
2026-01-26 11:25:02 +0600

    Add caching in home page

--- src/page-sections/market-1/JustForYouPeoducts/JustForYouProducts.tsx
+++ src/page-sections/market-1/JustForYouPeoducts/JustForYouProducts.tsx
@@ -2,8 +2,6 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import axios from "@lib/axiosClient";
-
 import Box from "@component/Box";
 import Card from "@component/Card";
 import FlexBox from "@component/FlexBox";
@@ -12,79 +10,35 @@ import CategorySectionCreator from "@component/CategorySectionCreator";
 import { currency } from "@utils/utils";
 import Rating from "@component/rating";
 import styles from "./JustForYouParoducts.module.css";
-import { Vortex } from "react-loader-spinner";
-import styled from "@emotion/styled";
 import { Chip } from "@component/Chip";
 import BeatLoader from "react-spinners/BeatLoader";
 import ApiBaseUrl from "api/ApiBaseUrl";
 import Image from "next/image";
-
-const LoaderWrapper = styled.div`
- display: flex;
- justify-content: center;
- align-items: center;
-`;
+import useFetcher from "@hook/useFetcher";
 
 const JustForYouProducts = () => {
- const [products, setProducts] = useState([]);
  const [currentPage, setCurrentPage] = useState(1);
- const [lastPage, setLastPage] = useState(1);
- const [loading, setLoading] = useState(false);
- const [loadingMore, setLoadingMore] = useState(false);
- const [isLoading, setIsLoading] = useState(false);
-
- const fetchProducts = async (page = 1) => {
-  if (loadingMore || loading) return;
-
-  setLoading(page === 1);
-  setLoadingMore(page > 1);
-  try {
-   const response = await axios.get(
-    `frontend/latest/justoforyou/product/view?page=${page}`
-   );
-   const data = response.data;
-
-   if (page === 1) {
-    setProducts(data.data);
-   } else {
-    setProducts((prevProducts) => [...prevProducts, ...data.data]);
-   }
-
-   setCurrentPage(data.current_page);
-   setLastPage(data.last_page);
-  } catch (error) {
-   console.error("Error fetching products:", error);
-  } finally {
-   setLoading(false);
-   setLoadingMore(false);
-  }
- };
+ const [products, setProducts] = useState([]);
+ const { data, isLoading } = useFetcher(
+  `frontend/latest/justoforyou/product/view?page=${currentPage}`
+ );
 
  useEffect(() => {
-  fetchProducts();
- }, []);
+  if (data?.data) {
+   if (currentPage > 1) {
+    setProducts((prevProducts) => [...prevProducts, ...data?.data]);
+   } else {
+    setProducts(data?.data || []);
+   }
+  }
+ }, [data]);
 
  const handleLoadMore = () => {
-  fetchProducts(currentPage + 1);
- };
-
- // Handle the product click to show loading state
- const handleProductClick = () => {
-  setIsLoading(true);
-  setTimeout(() => {
-   setIsLoading(false); // Optionally you can set a timeout if you want to stop loading after some delay
-  }, 1000); // Delay to show loading effect before redirecting
+  setCurrentPage(currentPage + 1);
  };
 
  return (
   <CategorySectionCreator title="Just For You">
-   {/* Show loading overlay when loading */}
-   {isLoading && (
-    <div className={styles.loadingOverlay}>
-     <div className={styles.loader}></div>
-    </div>
-   )}
-
    <Box my="-0.25rem">
     <FlexBox
      flexWrap="wrap"
@@ -92,10 +46,10 @@ const JustForYouProducts = () => {
      className={styles.flexContainer} // Use a custom class for better control
     >
      {products.length > 0 ? (
-      products.map((item) => (
+      products.map((item, i) => (
        <Box
         py="0.25rem"
-        key={item.product_slug}
+        key={i}
         className={styles.productCard} // Use a custom class for responsive sizing
        >
         <Card
@@ -109,7 +63,6 @@ const JustForYouProducts = () => {
            style={{
             padding: "0 0.5rem", // Added padding to prevent content touching the edges
            }}
-           onClick={handleProductClick}
           >
            {/* image cache  */}
            <div
@@ -171,58 +124,6 @@ const JustForYouProducts = () => {
            <Rating value={item.rating} outof={5} color="warn" readOnly />
           )}
 
-          {/* {item.discount_price == null && (
-                      <FlexBox>
-                        <H4
-                          fontWeight="600"
-                          fontSize="14px"
-                          color="primary.main"
-                        >
-                          {currency(item.seeling_price)}
-                        </H4>
-                      </FlexBox>
-                    )}
-
-                    {item.discount_price != null && (
-                      <FlexBox flexDirection="column">
-                        <H4 fontWeight="600" fontSize="14px" color="text.muted">
-                          BDT <del>{item.seeling_price}</del>
-                        </H4>
-                        <Box marginTop="4px">
-                          {" "}
-                        
-                          <H4
-                            fontWeight="600"
-                            fontSize="14px"
-                            color="primary.main"
-                          >
-                            {currency(item.discount_price)}
-                          </H4>
-                        </Box>
-                      </FlexBox>
-                    )} */}
-
-          {/* {item.discount_price == null && (
-        <FlexBox>
-          <H4 fontWeight="600" fontSize="14px" color="primary.main">
-            {currency(item.seeling_price)}
-          </H4>
-        </FlexBox>
-      )}
-
-      {item.discount_price != null && (
-        <FlexBox flexDirection="column" mt="0.25rem">
-          <H4 fontWeight="600" fontSize="14px" color="text.muted">
-            BDT <del>{item.seeling_price}</del>
-          </H4>
-          <Box>
-            <H4 fontWeight="600" fontSize="14px" color="primary.main">
-              {currency(item.discount_price)}
-            </H4>
-          </Box>
-        </FlexBox>
-      )} */}
-
           {item.seeling_price == 0 ? (
            <FlexBox>
             <H4 fontWeight="600" fontSize="14px" color="primary.main">
@@ -257,23 +158,15 @@ const JustForYouProducts = () => {
     </FlexBox>
    </Box>
 
-   {currentPage < lastPage && (
-    <div className={styles.buttonStyle}>
-     <button
-      className={styles.loadMore}
-      onClick={handleLoadMore}
-      disabled={loadingMore}
-     >
-      {loadingMore ? <BeatLoader size={18} color="#fff" /> : "Load More"}
-     </button>
-    </div>
-   )}
-
-   {loading && (
-    <LoaderWrapper>
-     <Vortex />
-    </LoaderWrapper>
-   )}
+   <div className={styles.buttonStyle}>
+    <button
+     className={styles.loadMore}
+     onClick={handleLoadMore}
+     disabled={isLoading}
+    >
+     {isLoading ? <BeatLoader size={18} color="#fff" /> : "Load More"}
+    </button>
+   </div>
   </CategorySectionCreator>
  );
 };
