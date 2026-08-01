af12ae39c48a082585cdf14e623cca27df75950c
dksaddy <saddypro1@gmail.com>
2026-08-01 12:23:55 +0600

    ApiBaseUrl sorted

--- src/api/ApiBaseUrl.js
+++ src/api/ApiBaseUrl.js
@@ -1,16 +1,18 @@
 const ApiBaseUrl = {
  // Live
-  baseUrl1: `https://frontend.tizaraa.com/api/v1/`,
-  baseUrl: `https://frontend.tizaraa.shop/api/v1/`,
-  ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
- localApiUrl: `https://customer-api.tizaraa.shop/api/v1/`,
+//  baseUrl1: `https://frontend.tizaraa.com/api/v1/`,
+//  baseUrl: `https://frontend.tizaraa.shop/api/v1/`,
+//  ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
+//  localApiUrl: `https://customer-api.tizaraa.shop/api/v1/`,
 
  // UAT
-//  baseUrl1: `https://frontend.tizaraa.com/api/`,
-//  baseUrl: `https://uat-client.tizaraa.shop/api/`,
-//  localApiUrl: `http://127.0.0.1:8000/api/v1/`,
-//  ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
+ baseUrl1: `https://frontend.tizaraa.com/api/`,
+ baseUrl: `https://uat-client.tizaraa.shop/api/`,
+ localApiUrl: `https://customer-api.tizaraa.shop/api/v1/`,
+ ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
 
+ // localApiUrl: `http://127.0.0.1:8000/api/v1/`,
+//for development purpose
 
 };
 
