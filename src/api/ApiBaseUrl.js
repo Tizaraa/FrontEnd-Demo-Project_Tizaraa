05f2a069ebf7a138489088bba80a53ce29994346
dksaddy <saddypro1@gmail.com>
2026-08-01 09:49:09 +0600

    Bump API base URLs to /v1/

--- src/api/ApiBaseUrl.js
+++ src/api/ApiBaseUrl.js
@@ -1,7 +1,7 @@
 const ApiBaseUrl = {
  // Live
-  baseUrl1: `https://frontend.tizaraa.com/api/`,
-  baseUrl: `https://frontend.tizaraa.shop/api/`,
+  baseUrl1: `https://frontend.tizaraa.com/api/v1/`,
+  baseUrl: `https://frontend.tizaraa.shop/api/v1/`,
   ImgUrl: `https://minio.tizaraa.shop/tizaraa/`,
  localApiUrl: `https://customer-api.tizaraa.shop/api/v1/`,
 
